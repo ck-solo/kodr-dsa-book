abe13c677ca5f849f20162ddcf145e1b6a6cfc9f
Lucy <solouser019@gmail.com>
2026-03-28 19:44:26 +0530

    complete 57 question of WeekTwo and update q 50

--- WeekOne/50.js
+++ WeekOne/50.js
@@ -2,13 +2,15 @@
 const prompt = require("prompt-sync")();
 let numb = prompt("Enter a numb: ");
 for (let i = 1; i <= numb; i++) {
-  let row = "";
-  for (let j = 1; j <= numb - 1; j++) {
-    row += " ";
-  }
-  for (let k = 1; k <= i; k++) {
-    row += "*";
-  }
+    let row = "";
+ 
+    for (let s = 1; s <= numb - i; s++) {
+        row += " ";
+    }
+ 
+    for (let j = 1; j <= i; j++) {
+        row += "*";
+    }
 
-  console.log(row);
+    console.log(row);
 }
